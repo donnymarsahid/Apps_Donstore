555794037c6f4dfee95732cc16ff5e8fd90764ba
unknown <donnymrshd.94@gmail.com>
2021-07-11 17:33:54 +0700

    slide slick and responsive

--- src/users/User.js
+++ src/users/User.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import logo from './img/logo.svg';
 import './css/Style.css';
 import api from '../api/database';
-import { Person, Cart3, Instagram, Linkedin, Github } from 'react-bootstrap-icons';
 import { Carousel } from 'react-bootstrap';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import women from './img/women.svg';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+
 const settings = {
   dots: true,
   infinite: false,
@@ -35,114 +37,91 @@ function User() {
 
   return (
     <>
-      <nav className="fixed-top shadow-sm">
-        <div class="nav-store">
-          <div class="profile">
-            <h3>
-              <Person size={35} className="profile-icons" />
-            </h3>
-          </div>
-          <div class="logo-nav-link">
-            <img src={logo} alt="donstore" className="donstore" />
-            <div class="link">
-              <a> Category</a>
-              <a> Store</a>
-              <a>Collection</a>
-            </div>
-          </div>
-          <div class="basket basket-icons">
-            <Cart3 size={30} className="basket-icons" /> (0)
-          </div>
-        </div>
-      </nav>
+      {/* Navbar Start */}
+      <Navbar />
+      {/* Navbar End */}
+      {/* Jumbotron Start */}
       <div class="jumbotron text-center">
         <h3 class="display-4">ONLY HERE !</h3>
         <p>Various kinds of fashion are here</p>
         <a class="btn btn-lg">SHOW NOW</a>
       </div>
-      <div class="category" style={{ margin: '10px' }}>
-        <div class="text-category text-center">
-          <h3>Category</h3>
-        </div>
-        <div class="row row-category  ">
-          {dataCategory.map((category) => {
-            return (
-              <div class="col-md-3">
-                <p className="text-center">{category.name}</p>
-                <div class="overlay">
-                  <a class="btn btn-lg">SHOW NOW</a>
+      {/* Jumbotron End */}
+      {/* Leading Brand Start */}
+      <div class="leading-brand">
+        <div class="container-fluid">
+          <div class="text-brand">
+            <h3>Leading Brand</h3>
+          </div>
+          <div class="row">
+            {dataLeadingBrand.map((brand) => {
+              return (
+                <div class="col-md-3">
+                  <img src={`http://localhost:3001/brand/${brand.images}`} alt={`http://localhost:3001/brand/${brand.images}`} />
+                  <p>{brand.name}</p>
+                  <p className="text-decoration-underline">Shop Now</p>
                 </div>
-                <img src={`http://localhost:3001/${category.images}`} alt={`http://localhost:3001/${category.images}`} />
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-      <div class="women-man">
+      {/* Leading Brand End */}
+      {/* Content Jumbotron Start */}
+      <div class="jumbotron-content">
         <Carousel fade activeIndex={index} onSelect={handleSelect}>
           <Carousel.Item>
             <div className="women d-block w-100" alt="First slide" />
-            <Carousel.Caption className="caption-women">
-              <div class="description">
-                <h3>there are some fashion</h3>
-                <a class="btn btn-style">
-                  <h3>FOR WOMEN</h3>
-                </a>
+            <Carousel.Caption className="container-carousel">
+              <div class="content">
+                <h3>fashion brand available</h3>
+                <a class="btn btn-lg">for women</a>
               </div>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <div className="man d-block w-100" alt="Second slide" />
-            <Carousel.Caption className="caption-women">
-              <div class="description">
-                <h3 className="fw-light">there are some fashion</h3>
-                <a class="btn btn-style">
-                  <h3>FOR MAN</h3>
-                </a>
+            <Carousel.Caption className="container-carousel">
+              <div class="content">
+                <h3>fashion brand available</h3>
+                <a class="btn btn-lg">for man</a>
               </div>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
-      <div class="leading-brand row-slick">
-        <div class="container-fluid">
-          <div class="text-leading ">
-            <h3>Leading Brand</h3>
+      {/* Content Jumbotron End */}
+      {/* Category Start */}
+      <div class="container-fluid">
+        <div class="category">
+          <div class="text-category">
+            <h3>Shop by Category</h3>
+          </div>
+          <div className="row-category">
+            <Slider {...settings} className="row">
+              {dataCategory.map((category) => {
+                return (
+                  <div class="col-md-12">
+                    <img src={`http://localhost:3001/${category.images}`} alt={`http://localhost:3001/${category.images}`} />
+                    <p className="text-decoration-underline text-uppercase">{category.name}</p>
+                  </div>
+                );
+              })}
+            </Slider>
           </div>
         </div>
-        {/* SLIDER START */}
-        <div className="row-leading-brand" style={{ margin: '10px' }}>
-          <Slider {...settings} className="row">
-            {dataLeadingBrand.map((brand) => {
-              return (
-                <div class="col-md-12">
-                  <img src={`http://localhost:3001/brand/${brand.images}`} alt={`http://localhost:3001/brand/${brand.images}`} width="100%" />
-                  <p>{brand.name}</p>
-                </div>
-              );
-            })}
-          </Slider>
-        </div>
-        {/* SLIDER END  */}
       </div>
-      <footer>
-        <div class="footer-content">
-          <div class="social-media">
-            <p>ABOUT US</p>
-            <p>CONTACT US</p>
-            <div class="social-media">
-              <Instagram size={30} className="me-3 icons-social" />
-              <Linkedin size={30} className="me-3 icons-social" />
-              <Github size={30} className="icons-social" />
-            </div>
-          </div>
-          <div class="copyright"> &copy; copyright Donny 2021 | all rights reserved</div>
-          <div class="privacy">
-            <p>PRIVACY POLICY</p>
-            <p>TERMS & CONDITIONS</p>
-          </div>
-        </div>
-      </footer>
+      {/* Category End */}
+      {/* Promo Start */}
+      <div class="promo text-center">
+        <p>Discount 20% | for first order</p>
+        <p>Register first to get a Discount 20%</p>
+        <button className="btn btn-sign-up">Sign Up</button>
+      </div>
+      {/* Promo End */}
+      {/* Footer Start */}
+      <Footer />
+      {/* Footer End */}
     </>
   );
 }
---
+++ src/users/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+import { Person, Cart3, List } from 'react-bootstrap-icons';
+import '../css/Style.css';
+import '../css/Responsive.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav className="fixed-top shadow-sm">
+        <div class="nav-store">
+          <div class="profile">
+            <h3>
+              <Person size={35} className="profile-icons" />
+            </h3>
+          </div>
+          <div class="logo-nav-link">
+            <img src={logo} alt="donstore" className="donstore" />
+            <div class="link">
+              <a>Category</a>
+              <a>Store</a>
+              <a>Collection</a>
+            </div>
+          </div>
+          <div class="basket basket-icons">
+            <Cart3 size={30} className="basket-icon" />
+            <p>(0)</p>
+          </div>
+          <div class="hamburger-menu" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+            <List size={30} />
+          </div>
+          {/* Hamburger Menu */}
+          <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div class="offcanvas-header" style={{ marginTop: '20px' }}>
+              <img style={{ width: '100px' }} src={logo} alt="donstore" className="offcanvas-title donstore" id="offcanvasScrollingLabel" />
+              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <hr />
+            <div class="offcanvas-body">
+              <a>Category</a>
+              <br />
+              <br />
+              <a>Store</a>
+              <br />
+              <br />
+              <a>Collection</a>
+              <br />
+              <hr />
+              <a>Account</a>
+            </div>
+          </div>
+          <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+                Backdroped with scrolling
+              </h5>
+              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <p>Try scrolling the rest of the page to see this option in action.</p>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
